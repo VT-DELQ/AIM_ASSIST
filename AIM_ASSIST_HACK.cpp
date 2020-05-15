03a4178e6993a3ad5f28ba5d2f9b94176cf2f38b
VT-DELQ <63230495+VT-DELQ@users.noreply.github.com>
2020-05-15 11:24:56 +0200

    Add files via upload

---
+++ AIM_ASSIST_HACK.cpp
@@ -0,0 +1,75 @@
+/*
+ *    
+ *    
+ *    _______ ___ ___ ___     _______ _______ _______ ___ _______ _______ 
+ *   |   _   |   |   Y   |   |   _   |   _   |   _   |   |   _   |       |
+ *   |.  1   |.  |.      |   |.  1   |   1___|   1___|.  |   1___|.|   | |
+ *   |.  _   |.  |. \_/  |   |.  _   |____   |____   |.  |____   `-|.  |-'
+ *   |:  |   |:  |:  |   |   |:  |   |:  1   |:  1   |:  |:  1   | |:  |  
+ *   |::.|:. |::.|::.|:. |   |::.|:. |::.. . |::.. . |::.|::.. . | |::.|  
+ *   `--- ---`---`--- ---'   `--- ---`-------`-------`---`-------' `---'  
+ *    ___ ___ _______ _______ ___ ___                                     
+ *   |   Y   |   _   |   _   |   Y   )                                    
+ *   |.  1   |.  1   |.  1___|.  1  /                                     
+ *   |.  _   |.  _   |.  |___|.  _  \                                     
+ *   |:  |   |:  |   |:  1   |:  |   \                                    
+ *   |::.|:. |::.|:. |::.. . |::.| .  )                                   
+ *   `--- ---`--- ---`-------`--- ---'                                    
+ *    _______ _______ ______  _______ _______ ___     _______             
+ *   |   _   |   _   |   _  \|   _   |   _   |   |   |   _   |            
+ *   |.  1___|.  |   |.  |   |   1___|.  |   |.  |   |.  1___|            
+ *   |.  |___|.  |   |.  |   |____   |.  |   |.  |___|.  __)_             
+ *   |:  1   |:  1   |:  |   |:  1   |:  1   |:  1   |:  1   |            
+ *   |::.. . |::.. . |::.|   |::.. . |::.. . |::.. . |::.. . |            
+ *   `-------`-------`--- ---`-------`-------`-------`-------'                           
+ *
+ *
+ *
+ *   AIM ASSIST HACK FOR CONSOLE 
+ *                                                                              
+ *   Is your K/D ratio literally killing you? Desperate battle 
+ *   times could call for desperate measures, and we all know how 
+ *   tempting it can be to look for cheats to stay locked on.
+ *
+ *   But there are more legit ways to improve your game. 
+ *   With Samsung QLED TV you can aim and hit your target 
+ *   like a pro player, without cheating.
+ *
+ *   As video games generates their frames in real time and creates 
+ *   a varying number of FPS, QLED’s FreeSync VRR technology enables 
+ *   each frame to be sent instantly and completely to the display. 
+ *   This will provide you with a clean, smooth animation and the 
+ *   possibility to eliminate both stutter, screen tearing and 
+ *   your enemies.
+ *
+ *   Smoother animation, better aim. A smart hack for any 
+ *   console player who wants to get the upper hand without 
+ *   the fear of being banned.
+ *
+ *
+ *         
+ *      ----------------------------------------------------------                                                                
+ *     |                                                          |
+ *     |                                                          |
+ *     |                                                          |
+ *     |                                                          |
+ *     |              Put an end to stuttering frames             |	
+ *     |                                                          |
+ *     |                                                          |
+ *     |                 and improve your game with               |
+ *     |                                                          |
+ *     |                                                          |
+ *     |                --->  Samsung QLED TV  <---               |
+ *     |                                                          |
+ *     |                                                          |  
+ *     |                                                          |
+ *     |                                                          |  
+ *      ----------------------------------------------------------
+ *              /\                                    /\      
+ *
+ *
+ *        https://www.samsung.com/se/tvs/all-tvs/?qled-4k+qled-8k
+ *                                                                       
+ */
+
+
---
+++ README_AIM_ASSIST.txt
@@ -0,0 +1,14 @@
+INSTRUCTION TO AIM ASSIST HACK
+
+1. Navigate to https://www.samsung.com/se/tvs/all-tvs/?qled-4k+qled-8k
+2. Select what size of Q800T or Q80T you prefer
+3. Proceed to checkout
+4. Fill in card details
+5. Wait for delivery
+
+INSTALLATION
+
+1. Connect and start your game console (your QLED will automatically switch to Game Mode)
+2. Start playing
+3. Enjoy smoother animation with better aim
+4. Take ‘em down.
